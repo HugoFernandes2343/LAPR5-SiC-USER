7379c75aa72444ce35d2c3c636b9ffcb8fb4e9e5
Norberto Sousa <1120608@isep.ipp.pt>
2017-10-27 16:03:15 +0100

    Estrutura do projeto

--- GameProject/src/gameproject/Character.java
+++ GameProject/src/gameproject/Character.java
@@ -5,6 +5,8 @@
  */
 package gameproject;
 
+import java.util.Objects;
+
 /**
  *
  * @author hugod
@@ -12,4 +14,59 @@ package gameproject;
 public class Character {
     private String name;
     private int strength;
+
+    public Character(String name, int strength) {
+        this.name = name;
+        this.strength = strength;
+    }
+
+    public Character() {
+        this.name = "";
+        this.strength = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+    
+        @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Character)) {
+            return false;
+        }
+        
+        Character that = (Character) obj;
+        if(strength != that.strength){
+            return false;
+        }
+        
+        return name == that.name;
+        
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.name);
+        hash = 59 * hash + this.strength;
+        return hash;
+    }
+
+    
+    
 }
--- GameProject/src/gameproject/Roads.java
+++ GameProject/src/gameproject/Roads.java
@@ -1,14 +1,83 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gameproject;
+
+import java.util.Objects;
 
 /**
  *
  * @author hugod
  */
 public class Roads {
+
     private int difficulty;
+    private Locale first;
+    private Locale second;
+
+    public Roads(int difficulty, Locale first, Locale second) {
+        this.difficulty = difficulty;
+        this.first = first;
+        this.second = second;
+    }
+
+    public Roads() {
+        this.difficulty = 0;
+        this.first = new Locale();
+        this.second = new Locale();
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Locale getFirst() {
+        return first;
+    }
+
+    public void setFirst(Locale first) {
+        this.first = first;
+    }
+
+    public Locale getSecond() {
+        return second;
+    }
+
+    public void setSecond(Locale second) {
+        this.second = second;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + this.difficulty;
+        hash = 97 * hash + Objects.hashCode(this.first);
+        hash = 97 * hash + Objects.hashCode(this.second);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Roads other = (Roads) obj;
+        if (this.difficulty != other.difficulty) {
+            return false;
+        }
+        if (!Objects.equals(this.first, other.first)) {
+            return false;
+        }
+        if (!Objects.equals(this.second, other.second)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
 }
